094771dab2e0a212291f059599d3197a0bc51b54
unknown <zaaferkenza08@gmail.com>
2023-10-30 23:34:02 +0100

    'entities'

---
+++ YourQuiz/src/test/java/com/example/yourquiz/YourQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
